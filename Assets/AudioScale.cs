0baaab468946410c8dbf4bfa2d5f32eafbec5572
johncleyton <fredscholiveira@uol.com.br>
2026-05-20 12:32:48 -0300

    MicrofoneAdicionado

---
+++ Assets/AudioScale.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScale : MonoBehaviour
+{
+    public Vector3 minScale, maxScale;
+    public DetectorAudio detector;
+
+    public float audioSens;
+    public float threshold = 0.1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float loudness = detector.getLoudnessMic() * audioSens;
+        if (loudness < threshold)
+            loudness = 0;
+        
+        transform.localScale = Vector3.Lerp(minScale, maxScale, loudness);
+    }
+}
