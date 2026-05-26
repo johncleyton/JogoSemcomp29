de76e32a481ae7e64e6bcdb0401c41ecd9361288
GuilhermePereiraSa <guilherme1478.wrath@gmail.com>
2026-05-26 07:16:18 -0300

    Ingredients data, joystick, collect ingredients added

--- Assets/Modules_Sudeste/MG_PaoDeQueijo/Scripts/Controller/CollectIngredients.cs
+++ Assets/Modules_Sudeste/MG_PaoDeQueijo/Scripts/Controller/CollectIngredients.cs
@@ -9,9 +9,13 @@ public class CollectIngredients : MonoBehaviour
     public List<IngredientData> ingredients;
 
     public int currentQuantity;
-    
+
     // para chamar em outro script
     public static CollectIngredients instance;
+
+    public Transform mesaTransform;
+    public GameObject iconPaoPrefab;
+    public GameObject iconQueijoPrefab;
 
     void Start()
     {
@@ -24,7 +28,7 @@ public class CollectIngredients : MonoBehaviour
     {
         // mostrar o "inventario" na mesa;
 
-        // fazer tuplas de objetos, PAO + QUEIJO        
+        // fazer tuplas de objetos, PAO + QUEIJO
 
     }
 
@@ -32,25 +36,47 @@ public class CollectIngredients : MonoBehaviour
     public void AddIngredient(IngredientData data)
     {
         ingredients.Add(data);
+
+        GameObject novoIcon = null;
+
+        if (data.nameIngredient == "Pao") novoIcon = iconPaoPrefab;
+        else if (data.nameIngredient == "Pao") novoIcon = iconQueijoPrefab;
+
+        if (novoIcon != null && mesaTransform != null)
+        {
+            Instantiate(novoIcon, mesaTransform);
+        }
     }
 
 
     public void VerifyRevenue()
     {
-        if(ingredients.Count == 2)
+        if (ingredients.Count == 2)
         {
             string i1 = ingredients[0].nameIngredient;
             string i2 = ingredients[1].nameIngredient;
 
-            if ((i1 == "Pao" && i2 == "Queijo") || (i1 == "Queijo" && i2 == "Pao")) {
-            Debug.Log("Pão de Queijo Perfeito!");
-            // Adiciona pontos
-            } else if (i1 == "Pao" && i2 == "Pao") {
-                Debug.Log("PÃO DE PÃO! Muito duro!"); // Reação de muito duro [cite: 69]
-            } else if (i1 == "Queijo" && i2 == "Queijo") {
-                Debug.Log("Muito Mole! Derretido!"); // Reação de muito mole [cite: 69]
+            if ((i1 == "Pao" && i2 == "Queijo") || (i1 == "Queijo" && i2 == "Pao"))
+            {
+                Debug.Log("Pão de Queijo Perfeito!");
+                // Adiciona pontos
             }
+            else if (i1 == "Pao" && i2 == "Pao")
+            {
+                Debug.Log("PÃO DE PÃO! Muito duro!"); // Reação de muito duro
+            }
+            else if (i1 == "Queijo" && i2 == "Queijo")
+            {
+                Debug.Log("Muito Mole! Derretido!"); // Reação de muito mole
+            }
+
             ingredients.Clear(); // Limpa a mesa para a próxima rodada
+
+
+            foreach (Transform child in mesaTransform)
+            {
+                Destroy(child.gameObject);
+            }
         }
     }
 
