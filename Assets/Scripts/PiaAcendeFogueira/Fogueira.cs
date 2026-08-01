4cc45946b5cf434ba684bba931fff522851344a9
GuilhermePereiraSa <guilherme1478.wrath@gmail.com>
2026-07-31 21:59:33 -0300

    pia acende a fogueira basics

---
+++ Assets/Scripts/PiaAcendeFogueira/Fogueira.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fogueira : MonoBehaviour
+{
+
+
+    public MicInput micInput;
+
+    public Transform fogoSprite;
+
+    public float timeLimit = 6f;
+
+    private float currentFire = 0f;
+    private float timer = 0f;
+
+    private bool gameOver = false;
+
+
+    public float blowThreshold = 0.5f;
+    public float fireGrowth = 25f;
+
+    public float fireDecay = 10f;
+
+    public float maxFireScale = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fogoSprite.localScale = Vector3.one * 0.1f;
+    }
+
+    
+    private void checkWin()
+    {
+        if(currentFire >= 100f)
+        {
+            gameOver = true;
+            // vitoria
+            Debug.Log("Fogueira acesa!");
+
+        }else if (timer >= timeLimit)
+        {
+            gameOver = true;
+
+
+            // derrota
+            Debug.Log("QUE DEMORA PIÁ!!!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameOver) return;
+
+        timer += Time.deltaTime;
+
+        if(micInput.loudness > blowThreshold)
+        {
+            currentFire += fireGrowth * Time.deltaTime;
+        }
+        else
+        {
+            currentFire -= fireDecay * Time.deltaTime;
+        }
+
+
+        float mappedScale = Mathf.Lerp(0.1f, maxFireScale, currentFire / 100f);
+
+        fogoSprite.localScale = Vector3.one * mappedScale;
+
+
+        checkWin();
+    }
+
+}
---
+++ Assets/Scripts/PiaAcendeFogueira/MicInput.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.PackageManager.UI;
+using UnityEngine;
+using UnityEngine.iOS;
+
+public class MicInput : MonoBehaviour
+{
+    public float sensitivity = 100f;
+    public int sampleWindow = 64; //quantidade amostras para calc volume audio
+
+
+    public float loudness = 0f;
+
+    private AudioClip micClip;
+
+    private string deviceName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        // algum microfone conectado
+        if(Microphone.devices.Length > 0)
+        {   
+            //primeiro conectar - padrao
+            deviceName = Microphone.devices[0];
+
+            // name, loopado, 20 seg, freq superior
+            micClip = Microphone.Start(deviceName, true, 20, 44100);
+
+        }
+        else
+        {
+            Debug.LogWarning("Nenhum microfone conectado. O jogo não funcionará como esperado.");
+        }
+
+    }
+
+
+    private float getLoudnessMicrophone()
+    {
+        int clipPos = Microphone.GetPosition(deviceName) - sampleWindow;
+
+        if(clipPos < 0 ) return 0f;
+
+        // n sampleWindow posicoes
+        float [] waveData = new float[sampleWindow];
+
+
+        micClip.GetData(waveData, clipPos);
+
+        float totalSqr = 0f;
+
+        for(int i=0; i < sampleWindow; i++)
+        {
+            totalSqr += waveData[i] * waveData[i];
+        }
+
+        return Mathf.Sqrt(totalSqr / sampleWindow);
+     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(micClip != null && Microphone.IsRecording(deviceName))
+        {
+            loudness = getLoudnessMicrophone() * sensitivity;
+        }
+    }
+
+
+
+    void onDisable()
+    {
+        if(deviceName != null && Microphone.IsRecording(deviceName))
+        {
+            // libera
+            Microphone.End(deviceName);
+        }
+    }
+
+}
